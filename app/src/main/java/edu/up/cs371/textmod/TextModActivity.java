cb892286eb925f9dd834a12348e3ec1da45088ce
mannj17 <mannj17@up.edu>
2016-02-03 17:47:17 -0800

    merged branches

--- app/src/main/java/edu/up/cs371/textmod/TextModActivity.java
+++ app/src/main/java/edu/up/cs371/textmod/TextModActivity.java
@@ -30,8 +30,9 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.Random;
 
-public class TextModActivity extends ActionBarActivity implements View.OnClickListener{
+public class TextModActivity extends ActionBarActivity implements View.OnClickListener {
 
     // array-list that contains our images to display
     private ArrayList<Bitmap> images;
@@ -48,7 +49,10 @@ public class TextModActivity extends ActionBarActivity implements View.OnClickLi
     protected Button lowerCase = null;
     String[] spinnerNames;
     protected Button clear = null;
-    protected EditText editTV= null;
+    protected EditText editTV = null;
+    protected Button randChar = null;
+    protected Random character;
+    String[] randoms;
 
     protected Button punctuation = null;
 
@@ -62,26 +66,25 @@ public class TextModActivity extends ActionBarActivity implements View.OnClickLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_mod);
 
-        clear = (Button)findViewById(R.id.clearButton); //sets the clear button and listener
+        clear = (Button) findViewById(R.id.clearButton); //sets the clear button and listener
         clear.setOnClickListener(this);
 
-        editTV = (EditText)findViewById(R.id.editText);
+        editTV = (EditText) findViewById(R.id.editText);
 
-        punctuation = (Button)findViewById(R.id.buttonNoPunctuation);
+        punctuation = (Button) findViewById(R.id.buttonNoPunctuation);
         punctuation.setOnClickListener(this);
 
-
         // set instance variables for our widgets
-        imageView = (ImageView)findViewById(R.id.imageView);
-        editText = (TextView)findViewById(R.id.editText);
-        editText = (TextView)findViewById(R.id.editText);
-        copyName = (Button)findViewById(R.id.copyName);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        editText = (TextView) findViewById(R.id.editText);
+        editText = (TextView) findViewById(R.id.editText);
+        copyName = (Button) findViewById(R.id.copyName);
         copyName.setOnClickListener(this);
 
         // Set up the spinner so that it shows the names in the spinner array resources
         //
         // get spinner object
-        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
         // get array of strings
         spinnerNames = getResources().getStringArray(R.array.spinner_names);
         // create adapter with the strings
@@ -100,13 +103,13 @@ public class TextModActivity extends ActionBarActivity implements View.OnClickLi
         // loop through, adding one image per string
         for (int i = 0; i < spinnerNames.length; i++) {
             // determine the index; use 0 if out of bounds
-            int id = imageIds2.getResourceId(i,0);
-            if (id == 0) id = imageIds2.getResourceId(0,0);
+            int id = imageIds2.getResourceId(i, 0);
+            if (id == 0) id = imageIds2.getResourceId(0, 0);
             // load the image; add to arraylist
             Bitmap img = BitmapFactory.decodeResource(getResources(), id);
             images.add(img);
 
-            reverse = (Button)findViewById(R.id.buttonReverse);
+            reverse = (Button) findViewById(R.id.buttonReverse);
             reverse.setOnClickListener(this);
 
         }
@@ -114,13 +117,18 @@ public class TextModActivity extends ActionBarActivity implements View.OnClickLi
         // define a listener for the spinner
         spinner.setOnItemSelectedListener(new MySpinnerListener());
 
-        upperCase = (Button)findViewById(R.id.upper);
+        upperCase = (Button) findViewById(R.id.upper);
         upperCase.setOnClickListener(this);
 
-        lowerCase = (Button)findViewById(R.id.lower);
+        lowerCase = (Button) findViewById(R.id.lower);
         lowerCase.setOnClickListener(this);
 
-        sentence = (EditText)findViewById(R.id.editText);
+        randChar = (Button) findViewById(R.id.randomCharacter);
+        randChar.setOnClickListener(this);
+
+        sentence = (EditText) findViewById(R.id.editText);
+
+        randoms = getResources().getStringArray(R.array.random_chars);
     }
 
     /**
@@ -153,70 +161,78 @@ public class TextModActivity extends ActionBarActivity implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.upper){
+        Random rand = new Random();
+        if (v.getId() == R.id.upper) {
             CharSequence temp = sentence.getText();
             String temp2 = temp.toString();
             temp2 = temp2.toUpperCase();
-            CharSequence temp3 = (CharSequence)temp2;
+            CharSequence temp3 = (CharSequence) temp2;
             sentence.setText(temp3);
         }
-        if(v.getId() == R.id.lower){
+        if (v.getId() == R.id.lower) {
             CharSequence temp = sentence.getText();
             String temp2 = temp.toString();
             temp2 = temp2.toLowerCase();
-            CharSequence temp3 = (CharSequence)temp2;
+            CharSequence temp3 = (CharSequence) temp2;
             sentence.setText(temp3);
         }
-        if((v.getId() == R.id.copyName)){
-            String val = (String)editText.getText().toString();
+        if ((v.getId() == R.id.copyName)) {
+            String val = (String) editText.getText().toString();
             editText.setText(val + spinnerNames[currentPostion]);
         }
-        if(v.getId() == R.id.clearButton){
+        if (v.getId() == R.id.clearButton) {
             editTV.setText("");
-            
+
         }
-        if(v.getId() == R.id.buttonReverse)
-        {
+        if (v.getId() == R.id.buttonReverse) {
             String input = this.editText.getText().toString();
             StringBuffer buffer = new StringBuffer(input);
             buffer.reverse();
             this.editText.setText(buffer);
         }
-        if(v.getId()== R.id.buttonNoPunctuation)
-        {
+        if (v.getId() == R.id.buttonNoPunctuation) {
             String test = this.editText.getText().toString();
-            test = test.replaceAll("[\\!\\.\\,\\?]","");
+            test = test.replaceAll("[\\!\\.\\,\\?]", "");
             this.editText.setText(test);
-
-        }
-             
- 
-    }
-
-    /**
-     * class that handles our spinner's selection events
-     */
-    private class MySpinnerListener implements OnItemSelectedListener {
-
-        /**
-         * @see android.widget.AdapterView.OnItemSelectedListener#onItemSelected(
-         *                  android.widget.AdapterView, android.view.View, int, long)
-         */
-        @Override
-        public void onItemSelected(AdapterView<?> parentView, View selectedItemView,
-                                   int position, long id) {
-            // set the image to the one corresponding to the index selected by the spinner
-            imageView.setImageBitmap(images.get(position));
-            currentPostion = position;
-        }
-
-        /**
-         * @see android.widget.AdapterView.OnItemSelectedListener#onNothingSelected(
-         *                  android.widget.AdapterView)
-         */
-        @Override
-        public void onNothingSelected(AdapterView<?> parentView) {
-            // your code here
+            if (v.getId() == R.id.randomCharacter) {
+                CharSequence temp = sentence.getText();
+                int tempChar = rand.nextInt(randoms.length);
+                int tempSpot = rand.nextInt(temp.length());
+                CharSequence temp2 = temp.subSequence(0, tempSpot);
+                String temp3 = temp2.toString();
+                CharSequence temp4 = temp.subSequence(tempSpot + 1, temp.length());
+                String temp5 = temp4.toString();
+                String finalTemp = temp3 + randoms[tempChar] + temp5;
+                CharSequence finalChar = (CharSequence) finalTemp;
+                sentence.setText(finalTemp);
+            }
         }
     }
+
+        /**
+         * class that handles our spinner's selection events
+         */
+        private class MySpinnerListener implements OnItemSelectedListener {
+
+            /**
+             * @see android.widget.AdapterView.OnItemSelectedListener#onItemSelected(
+             *android.widget.AdapterView, android.view.View, int, long)
+             */
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView,
+                                       int position, long id) {
+                // set the image to the one corresponding to the index selected by the spinner
+                imageView.setImageBitmap(images.get(position));
+                currentPostion = position;
+            }
+
+            /**
+             * @see android.widget.AdapterView.OnItemSelectedListener#onNothingSelected(
+             *android.widget.AdapterView)
+             */
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+        }
 }
